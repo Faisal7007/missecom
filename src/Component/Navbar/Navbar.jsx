95c31a8bc98c17fc2fd75c62450cc688fc89c56a
Meezan-cool <nmeezan4@gmail.com>
2023-09-01 00:25:45 +0530

    add

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -3,7 +3,22 @@ import './navbar.scss'
 const Navbar = () => {
   return (
     <div className='Navbar_Main'>
-        Navbar
+        <div className='Navbar_Sub'>
+          {/* Logo plus header */}
+                 <div className='Navbar_Logo'>
+                     Logo
+                 </div>
+                 {/* Navbar list */}
+            <div>
+                <div>Home</div>
+                <div>Design</div>
+                <div>Blog</div>
+            </div>
+            {/* Navbar icons  */}
+            <div>
+                icons
+            </div>
+        </div>
     </div>
   )
 }
